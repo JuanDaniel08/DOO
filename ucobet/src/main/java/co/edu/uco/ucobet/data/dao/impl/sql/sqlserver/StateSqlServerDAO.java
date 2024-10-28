3bfd21e2c2e16feb1e0757c8c54729dce2cbadb8
eltuttis <jrodriguezgiraldo8@gmail.com>
2024-10-28 00:54:20 -0500

    se agrega los archivos de businessLogic

--- ucobet/src/main/java/co/edu/uco/ucobet/data/dao/impl/sql/sqlserver/StateSqlServerDAO.java
+++ ucobet/src/main/java/co/edu/uco/ucobet/data/dao/impl/sql/sqlserver/StateSqlServerDAO.java
@@ -1,13 +1,19 @@
 package co.edu.uco.ucobet.data.dao.impl.sql.sqlserver;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.UUID;
 
 import co.edu.uco.ucobet.data.dao.StateDAO;
+import co.edu.uco.ucobet.data.dao.impl.sql.SqlDAO;
 import co.edu.uco.ucobet.entity.StateEntity;
 
 
-public class StateSqlServerDAO implements StateDAO {
+final public class StateSqlServerDAO extends SqlDAO implements StateDAO {
+
+	public StateSqlServerDAO(Connection connection) {
+		super(connection);
+	}
 
 	@Override
 	public StateEntity findByID(UUID id) {
